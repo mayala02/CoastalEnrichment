e83d33e54bc64414d0b7834b0f5097c70b3855de
Melanie Ayala <mayala03@syr.edu>
2023-06-06 20:38:09 -0400

    stats

--- Main.py
+++ Main.py
@@ -24,9 +24,15 @@ import numpy as np
 from scipy.ndimage import maximum_filter
 import earthpy.plot as ep
 import matplotlib.pyplot as plt
+import psycopg2
 
-#Takes a shapeFile (Site) and a json file(login)
-def get_Disp_Maps(Site,login):
+import subprocess
+from mintpy.cli import tsview
+from mintpy.cli import plot_transection
+import ipympl
+
+#Takes a shapeFile (Site), a json file(login), and the database connection
+def get_Disp_Maps(Site,login,conn):
     #First get the geometry of the shape file to use as the Polygon parameter for the ASF search
     geometry = str(Site['geometry'][0])
     date = str(Site['Date'][0])
@@ -61,9 +67,8 @@ def get_Disp_Maps(Site,login):
             end = end
         )
     except:
+        pass
 
-
-    print('Done')
     data = searchResults.geojson()
 
     #Extracts scenes from job search and creates list of all scene names
@@ -78,28 +83,45 @@ def get_Disp_Maps(Site,login):
             dates.append(date)
         scenes.reverse()
         dates.reverse()
-        df = pd.DataFrame(list(zip(dates, scenes)),columns =['Dates', 'SceneNames']).drop_duplicates()
+        df = pd.DataFrame(list(zip(dates, scenes)),columns =['Dates', 'SceneNames'])
         return df
-    scenes_dates= get_scene_name() #list with all scene names
-    scenes = list(scenes_dates['SceneNames'])
+    scenesList= get_scene_name() #list with all scene names
 
-    def create_jobs(data): 
+    filtered = scenesList.drop_duplicates()
+    scenes = list(filtered['SceneNames'])
+    dates = list(filtered['Dates'])
+
+    def create_jobs(data,date): 
         jobs = [] 
         count = len(data) 
         for i in range(count-1): 
             scene1 = data[i]
             scene2 = data[i+1]
+            if date[i+1].endswith('Z'):
+                sceneDate = datetime.strptime(date[i+1], '%Y-%m-%dT%H:%M:%S.%fZ').date()
+            else:
+                sceneDate = datetime.strptime(date[i+1], '%Y-%m-%dT%H:%M:%S.%f').date()
+            print(sceneDate)
             print(len(scene2))
-            insar_job = HyP3.prepare_insar_job(scene1, scene2, name = JobName, include_displacement_maps=True) 
+            insar_job = HyP3.prepare_insar_job(scene1, scene2, name = JobName, include_displacement_maps=True,include_dem=True,include_look_vectors=True) 
             print(insar_job)
-            jobs.append(insar_job)  
+            jobs.append(insar_job) 
+            pair = scene1+','+scene2
+            try:
+                cursor = conn.cursor()
+                cursor.execute("INSERT INTO project1 (pair, site, reference_date) VALUES(%s, %s, %s)", (pair, JobName, sceneDate))
+                conn.commit()
+                cursor.close()
+            except:
+                conn = psycopg2.connect(host='localhost',database = 'postgres', user='postgres',password = 'postgrespw',port=32768)
         return jobs 
-    jobsList = create_jobs(scenes)
+    jobsList = create_jobs(scenes, dates)
 
     hyp3 = HyP3(username = userName, password = pw) #authenticate using ASF credentials
     batch = hyp3.submit_prepared_jobs(prepared_jobs = jobsList)
 
     #Creates New Folder called "data" where the jobs will be downloaded
+    directory = os.getcwd()
     output = os.path.join(directory, 'rawData')
     if not os.path.exists(output):
         os.makedirs(output)
@@ -123,22 +145,47 @@ def get_Disp_Maps(Site,login):
 
 #Give function path to directory
 def crop_InSAR(directory):
+    output = os.path.join(directory, 'rawData')
+    folderList = os.listdir(output)
+    tifList = []
+    #Loop through the folders
+    for x in range(len(folderList)):
+        folderName = os.path.join(output, folderList[x])
+        fileList = os.listdir(folderName)
+        #Loop through files in folder
+        for file in fileList:
+            #Select file that ends in vert_disp.tif
+            if file.endswith(".tif"):
+                fileName = os.path.join(folderName, file)
+                tifList.append(fileName)
+            if file.startswith(folderList[x]+'.txt'):
+                fileName = os.path.join(folderName, file)
+                f = open(fileName,'r')
+                lines = f.readlines()
+                scene1 = lines[0].split(' ')
+                scene2 = lines[1].split(' ')
+            
+                pair = scene1[2].replace('\n','')+','+scene2[2].replace('\n','')
+                try:
+                    cursor = conn.cursor()
+                    cursor.execute("SELECT insar_name from project1 where insar_name = %s",(folderList[x],))
+                    row = cursor.fetchall()
+                    if row == []:
+                        cursor.execute("UPDATE project1 SET insar_name = %s where pair = %s",(folderList[x],pair))
+                        conn.commit()
+                    cursor.close()
+                except:
+                    conn = psycopg2.connect(host='localhost',database = 'postgres', user='postgres',password = 'postgrespw',port=32768)
 
-    #Setting variables for file system
-    data = os.path.join(directory, "rawData")
-    output = os.path.join(directory, "Masked")
-    #List of unprocessed geotifs:
-    dataList = os.listdir(data)
-
-    dataset = rio.open(os.path.join(data, dataList[0]))
+    dataset = rio.open(tifList[0])
     boundingBox = dataset.bounds
     left = boundingBox.left
     right = boundingBox.right
     top = boundingBox.top
     bottom = boundingBox.bottom
     print(boundingBox)
-    for x in range(1,len(dataList)):
-        dataset = rio.open(os.path.join(data, dataList[x]))
+    for x in range(1,len(tifList)):
+        dataset = rio.open(tifList[x])
         boundingBox = dataset.bounds
         if boundingBox.left < left:
             left = boundingBox.left
@@ -148,15 +195,16 @@ def crop_InSAR(directory):
             top = boundingBox.top
         if boundingBox.bottom < bottom:
             bottom = boundingBox.bottom
+        print(left,bottom,right,top)
     minbbox = (left,bottom,right,top)
-    print(minbbox)
 
     #Crop the water mask incase it does not have the min bounds
-    options = gdal.WarpOptions(options=['tr'], outputBounds=minbbox, format="GTiff")
     #For loop to crop all the tif files to the min bounds
     #all files need to be the same height and width to work with them in numpy
-    for geoTif in dataList:
-        dataset = gdal.Open(os.path.join(data, geoTif)) 
-        options = gdal.WarpOptions(options=options)
-        newfile = gdal.Warp('crop'+geoTif, dataset , options=options)
+    opts = gdal.WarpOptions(options=['tr'], outputBounds=minbbox, format="GTiff")
+    for geoTif in tifList:
+        dataset = gdal.Open(geoTif)
+        newfile = gdal.Warp(geoTif.split('.')[0]+'_crop.tif', dataset , options=opts)
         dataset = None #close the file
+
+database = psycopg2.connect(host='localhost',database = 'postgres', user='postgres',password = 'postgrespw',port=32768)
